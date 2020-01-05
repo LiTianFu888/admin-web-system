70362c604b821099a87b74467f60efae9951f17c
aliyun <18811610626@163.com>
2020-01-05 14:45:41 +0800

    基础框架succ

---
+++ application/controllers/Home.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Home extends CI_Controller{
+	public function index(){
+		$data['test'] = 'data from hello';
+		$this->load->view('home',$data);
+	}
+}
