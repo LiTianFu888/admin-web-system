52d268125fa124933df4ba829a436ce14c4c3557
aliyun <18811610626@163.com>
2020-01-06 22:35:55 +0800

    商品view

--- application/views/manage.php
+++ application/views/manage.php
@@ -1,6 +1,3 @@
-
-
- 
  
 <!DOCTYPE html>
 <html>
@@ -10,7 +7,6 @@
 <title>layui客户后台系统</title>
     <link rel="stylesheet" href="<?php echo base_url().'static/common/layui/css/layui.css'?>">
     <link rel="stylesheet" href="<?php echo base_url().'static/admin/css/login.css'?>">
-    <script src="<?php echo base_url().'static/common/layui/layui.js'?>"></script>
 </head>
 <body class="layui-layout-body">
 <div class="layui-layout layui-layout-admin">
@@ -20,7 +16,6 @@
     <ul class="layui-nav layui-layout-left">
       <li class="layui-nav-item"><a href="">控制台</a></li>
       <li class="layui-nav-item"><a href="">商品管理</a></li>
-      <li class="layui-nav-item"><a href="">用户</a></li>
       <li class="layui-nav-item">
         <a href="javascript:;">其它系统</a>
         <dl class="layui-nav-child">
@@ -42,7 +37,7 @@
           <dd><a href="">安全设置</a></dd>
         </dl>
       </li>
-      <li class="layui-nav-item"><a href="">退了</a></li>
+      <li class="layui-nav-item"><a href="">logout</a></li>
     </ul>
   </div>
   
@@ -62,29 +57,58 @@
           <a href="javascript:;">订单管理</a>
           <dl class="layui-nav-child">
             <dd><a href="javascript:;">订单查询</a></dd>
-            <dd><a href="javascript:;">列表二</a></dd>
-            <dd><a href="">超链接</a></dd>
           </dl>
         </li>
         <li class="layui-nav-item">
           <a href="javascript:;">库存管理</a>
           <dl class="layui-nav-child">
             <dd><a href="javascript:;">库存查询</a></dd>
-            <dd><a href="javascript:;">列表二</a></dd>
-            <dd><a href="">超链接</a></dd>
           </dl>
         </li>
-        <li class="layui-nav-item"><a href="">云市场</a></li>
-        <li class="layui-nav-item"><a href="">发布商品</a></li>
       </ul>
     </div>
   </div>
   
   <div class="layui-body">
     <!-- 内容主体区域 -->
-    <div style="padding: 15px;">内容主体区域</div>
-  </div>
-  
+	<div class ="layui-form-item">
+
+	</div>	
+	<div>
+	<form class="layui-form" action="/order/filter" lay-filter="example"> 
+  	<div class="layui-form-item">
+    	<label class="layui-form-label">商品ID</label>
+    	<div class="layui-input-inline">
+      	<input  id = "orderid" name="name" class="layui-input" type="text" placeholder="请输入商品id" autocomplete="off" lay-verify="title">
+    	</div>
+    	<div class="layui-input-inline">
+      	<button id = "search" class="layui-btn"  lay-filter="demo1" lay-submit="">查询</button>
+    	</div>
+  	</div>
+	</form>
+	<div class="layui-btn-group demoTable">
+  		<button class="layui-btn" data-type="getCheckData">获取选中行数据</button>
+	  	<button class="layui-btn" data-type="getCheckLength">获取选中数目</button>
+  		<button class="layui-btn" data-type="isAll">验证是否全选</button>
+	</div>
+   <div style="padding: 15px;">
+    <table  id="table1"  class="layui-table" lay-filter="demo" lay-data="{width: 892, height:330, url:'/goods/table', page:true, id:'idTest'}">
+    <thead>
+    <tr>
+      <th lay-data="{type:'checkbox', fixed: 'left'}"></th>
+      <th lay-data="{field:'id', width:160, sort: true, fixed: true}">商品ID</th>
+      <th lay-data="{field:'name', width:160}">名称</th>
+      <th lay-data="{field:'brand', width:160, sort: true}">品牌</th>
+      <th lay-data="{field:'size', width:160, sort: true}">尺码</th>
+      <th lay-data="{field:'gender', width:80}">款式</th>
+      <th lay-data="{field:'offset', width:160}">是否下架</th>
+      <th lay-data="{field:'add_time', width:200}">添加时间</th>
+      <th lay-data="{field:'price', width:80, sort: true, fixed: 'right'}">价格</th>
+      <th lay-data="{fixed: 'right', width:178, align:'center', toolbar: '#barDemo'}">操作</th>
+    </tr>
+  </thead> 
+</table>
+    </div>
   <div class="layui-footer">
     <!-- 底部固定区域 -->
     © layui.com - 底部固定区域
@@ -92,10 +116,70 @@
 </div>
 <script src="<?php echo base_url().'static/common/layui/layui.js'?>"></script>
 <script>
+layui.use('table', function(){
+  var table = layui.table;
+  //监听表格复选框选择
+  table.on('checkbox(demo)', function(obj){
+    console.log(obj)
+  });
+  //监听工具条
+  table.on('tool(demo)', function(obj){
+    var data = obj.data;
+    if(obj.event === 'detail'){
+      layer.msg('ID：'+ data.id + ' 的查看操作');
+    } else if(obj.event === 'del'){
+      layer.confirm('确认下架？', function(index){
+      	           $.ajax({
+                        url: "/goods/del",
+                        type: "POST",
+                        data: {id: data.id},
+                        success: function (msg) {
+                            if (msg == 200) {
+                                //删除这一行
+                                //关闭弹框
+                                layer.close(index);
+                                layer.msg("下架成功！请刷新", {icon: 6});
+                            } else {
+                                layer.msg("下架失败", {icon: 5});
+                            }
+                        }
+                    });
+	});
+    } else if(obj.event === 'edit'){
+      layer.alert('编辑行：<br>'+ JSON.stringify(data))
+    }
+  });
+  
+  var $ = layui.$, active = {
+    getCheckData: function(){ //获取选中数据
+      var checkStatus = table.checkStatus('idTest')
+      ,data = checkStatus.data;
+      layer.alert(JSON.stringify(data));
+    }
+    ,getCheckLength: function(){ //获取选中数目
+      var checkStatus = table.checkStatus('idTest')
+      ,data = checkStatus.data;
+      layer.msg('选中了：'+ data.length + ' 个');
+    }
+    ,isAll: function(){ //验证是否全选
+      var checkStatus = table.checkStatus('idTest');
+      layer.msg(checkStatus.isAll ? '全选': '未全选')
+    }
+  };
+  
+  $('.demoTable .layui-btn').on('click', function(){
+    var type = $(this).data('type');
+    active[type] ? active[type].call(this) : '';
+  });
+});
+</script>
+<script id="barDemo" type="text/html">
+  <a  class="layui-btn layui-btn-danger layui-btn-xs" lay-event="del">下架</a>
+</script>
+<script>
 //JavaScript代码区域
 layui.use('element', function(){
   var element = layui.element;
-  
 });
 </script>
 </body>
